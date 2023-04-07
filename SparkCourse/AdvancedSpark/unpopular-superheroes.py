01ec8093842769d80f9ddfc4892e770629a177d8
joorgemartinez <martinezca.jorge@gmail.com>
2023-04-07 19:22:19 +0200

    Adding comments

--- SparkCourse/AdvancedSpark/unpopular-superheroes.py
+++ SparkCourse/AdvancedSpark/unpopular-superheroes.py
@@ -1,35 +1,34 @@
+# Import necessary modules
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as func
 from pyspark.sql.types import StructType, StructField, IntegerType, StringType
 
-# Create a SparkSession with an app name "LeastPopularSuperhero"
+# Create a SparkSession
 spark = SparkSession.builder.appName("LeastPopularSuperhero").getOrCreate()
 
-# Define a schema for the names dataset, which has two fields: id and name
+# Define the schema for the Marvel names dataset
 schema = StructType([ \
                      StructField("id", IntegerType(), True), \
                      StructField("name", StringType(), True)])
 
-# Load the names dataset into a Spark DataFrame, using the defined schema
+# Load the Marvel names dataset into a DataFrame using the defined schema
 names = spark.read.schema(schema).option("sep", " ").csv("Datasets/Marvel-names.txt")
 
-# Load the graph dataset into a Spark DataFrame, where each line represents a co-appearance
-# of two superheroes in a comic book
+# Load the Marvel graph dataset into a DataFrame
 lines = spark.read.text("Datasets/Marvel-graph.txt")
 
-# Parse the graph dataset to create a new DataFrame with columns for superhero ID and their number of co-appearances
+# Split the values in the Marvel graph DataFrame by spaces and extract the superhero ID and number of connections
 connections = lines.withColumn("id", func.split(func.trim(func.col("value")), " ")[0]) \
     .withColumn("connections", func.size(func.split(func.trim(func.col("value")), " ")) - 1) \
     .groupBy("id").agg(func.sum("connections").alias("connections"))
 
-
-
+# Find the minimum number of connections and filter for superheroes with that number of connections
 minConnectionsCount = connections.agg(func.min("connections")).first()[0]
 minConnections = connections.filter(func.col("connections") == minConnectionsCount)
 
-
+# Join the DataFrame of superheroes with the minimum number of connections with the names DataFrame to get their names
 minConnectionsWithName = minConnections.join(names, "id")
 
+# Print the result
 print(f"The following characters have only {str(minConnectionsCount)} co-appearances.")
-
 minConnectionsWithName.select("name").show()
